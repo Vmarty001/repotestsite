c0067b65ad7c727c11432bdf0a45c1b25d9347a2
vmay <va>
2023-11-11 22:07:17 +0300

    first commit

---
+++ src/components/Header/Logo.js
@@ -0,0 +1,19 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import img2 from './vmaylogo2.png';
+
+
+
+
+
+
+
+const Logo = () => {
+    return(
+
+            <img src={img2} alt="The Weirdos" />
+    )
+}
+
+export default Logo
