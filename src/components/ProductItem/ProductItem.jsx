f2e25032bccd585a9e6c2c30a0f335efb1b17d65
vmay <va>
2024-05-12 19:09:27 +0300

    changed

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,72 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import Button from '../Button/Button';
+import React from 'react';
+import Button from "../Button/Button";
 import './ProductItem.css';
 
-const ProductItem = ({ product, className, onAdd, onRemove }) => {
-    const [selectedSize, setSelectedSize] = useState(null);
-    const [selectedProduct, setSelectedProduct] = useState(null);
-    const [inCart, setInCart] = useState(false);
-    const [minPrice, setMinPrice] = useState(null);
-
-    useEffect(() => {
-        if (selectedSize) {
-            setSelectedProduct({ ...product, selectedSize, price: product.prices[selectedSize] });
-        } else {
-            setSelectedProduct(null);
-        }
-
-        // Вычисляем минимальную цену среди всех размеров
-        const prices = Object.values(product.prices);
-        setMinPrice(Math.min(...prices));
-    }, [product, selectedSize]);
-
-    const onSizeSelect = (size) => {
-        setSelectedSize(size);
-    };
+const ProductItem = ({product, className, onAdd}) => {
 
     const onAddHandler = () => {
-        if (selectedProduct) {
-            onAdd(selectedProduct);
-            setInCart(true);
-        } else {
-            alert('Выберите размер');
-        }
-    };
-
-    const onRemoveHandler = () => {
-        if (selectedProduct) {
-            onRemove(selectedProduct.id);
-            setInCart(false);
-        }
-    };
+        onAdd(product);
+    }
 
     return (
         <div className={'product ' + className}>
-            <div className={'img'}>
-                <img src={product.img} alt={product.title} />
-            </div>
+            <div className={'img'}/>
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
             <div className={'price'}>
-                <span><b>{selectedSize ? product.prices[selectedSize] : `От ${minPrice}`}</b></span>
+                <span>Стоимость: <b>{product.price}</b></span>
             </div>
-            <div className={'sizes'}>
-                {product.sizes && (
-                    <div>
-                        Размеры: {product.sizes.map((size) => (
-                        <button
-                            key={size}
-                            onClick={() => onSizeSelect(size)}
-                            className={selectedSize === size ? 'selected' : ''}
-                        >
-                            {size}
-                        </button>
-                    ))}
-                    </div>
-                )}
-            </div>
-            <Button className={'add-btn'} onClick={inCart ? onRemoveHandler : onAddHandler}>
-                {inCart ? 'Убрать из корзины' : 'Добавить в корзину'}
+            <Button className={'add-btn'} onClick={onAddHandler}>
+                Добавить в корзину
             </Button>
         </div>
     );
--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -1,127 +1,83 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, {useState} from 'react';
 import './ProductList.css';
-import ProductItem from '../ProductItem/ProductItem';
-import { useTelegram } from '../../hooks/useTelegram';
-import Nike from './images/pinknike.jpg';
-import Nike1 from './images/2023-11-10 23.55.38.jpg';
-import Nike3 from './images/2023-11-10 23.55.33.jpg';
-import Nike4 from './images/2023-11-10 23.55.23.jpg';
-import Nike5 from './images/2023-11-10 23.55.27.jpg';
-import Nike6 from './images/2023-11-10 23.55.19.jpg';
-import Nike7 from './images/2023-11-10 23.55.15.jpg';
-import Nike8 from './images/2023-11-10 23.55.10.jpg';
+import ProductItem from "../ProductItem/ProductItem";
+import {useTelegram} from "../../hooks/useTelegram";
+import {useCallback, useEffect} from "react";
 
 const products = [
-  { id: '1', title: 'Название1', prices: { '41': 5000, '42': 6000, '43': 7000 }, description: 'Синего цвета, прямые', img: Nike, sizes: ['41', '42', '43'], category: 'Новое' },
-  { id: '2', title: 'Название1', prices: { '41': 12000, '42': 13000, '43': 14000 }, description: 'Зеленого цвета, теплая', img: Nike1, sizes: ['41', '42', '43'], category: 'Кроссовки' },
-  { id: '3', title: 'Название 2', prices: { '41': 5000, '42': 6000, '43': 7000 }, description: 'Синего цвета, прямые', img: Nike3, sizes: ['41', '42', '43'], category: 'Новое' },
-  { id: '4', title: 'Название 8', prices: { '41': 122, '42': 133, '43': 144 }, description: 'Зеленого цвета, теплая', img: Nike4, sizes: ['41', '42', '43'], category: 'Одежда' },
-  { id: '5', title: 'Название 3', prices: { '41': 5000, '42': 6000, '43': 7000 }, description: 'Синего цвета, прямые', img: Nike5, sizes: ['41', '42', '43'], category: 'Одежда' },
-  { id: '6', title: 'Название 7', prices: { '41': 600, '42': 700, '43': 800 }, description: 'Зеленого цвета, теплая', img: Nike6, sizes: ['41', '42', '43'], category: 'Кроссовки' },
-  { id: '7', title: 'Название 4', prices: { '41': 5500, '42': '6500', '43': 7500 }, description: 'Синего цвета, прямые', img: Nike7, sizes: ['41', '42', '43'], category: 'Новое' },
-  { id: '8', title: 'Название 5', prices: { '41': 12000, '42': 13000, '43': 14000 }, description: 'Зеленого цвета, теплая', img: Nike8, sizes: ['41', '42', '43'], category: 'Одежда' },
-];
+  {id: '1', title: 'Джинсы', price: 5000, description: 'Синего цвета, прямые'},
+  {id: '2', title: 'Куртка', price: 12000, description: 'Зеленого цвета, теплая'},
+  {id: '3', title: 'Джинсы 2', price: 5000, description: 'Синего цвета, прямые'},
+  {id: '4', title: 'Куртка 8', price: 122, description: 'Зеленого цвета, теплая'},
+  {id: '5', title: 'Джинсы 3', price: 5000, description: 'Синего цвета, прямые'},
+  {id: '6', title: 'Куртка 7', price: 600, description: 'Зеленого цвета, теплая'},
+  {id: '7', title: 'Джинсы 4', price: 5500, description: 'Синего цвета, прямые'},
+  {id: '8', title: 'Куртка 5', price: 12000, description: 'Зеленого цвета, теплая'},
+]
 
 const getTotalPrice = (items = []) => {
   return items.reduce((acc, item) => {
-    return (acc += item.price);
-  }, 0);
-};
+    return acc += item.price
+  }, 0)
+}
 
 const ProductList = () => {
   const [addedItems, setAddedItems] = useState([]);
-  const [activeCategory, setActiveCategory] = useState('Новое');
-  const [filteredProducts, setFilteredProducts] = useState([]);
-  const { tg, queryId } = useTelegram();
+  const {tg, queryId} = useTelegram();
 
   const onSendData = useCallback(() => {
     const data = {
       products: addedItems,
       totalPrice: getTotalPrice(addedItems),
       queryId,
-    };
-    fetch('http://45.89.188.162:8000/web-data', {
+    }
+    fetch('http://85.119.146.179:8000/web-data', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(data),
-    });
-  }, [addedItems, queryId]);
+      body: JSON.stringify(data)
+    })
+  }, [addedItems])
 
   useEffect(() => {
-    tg.onEvent('mainButtonClicked', onSendData);
+    tg.onEvent('mainButtonClicked', onSendData)
     return () => {
-      tg.offEvent('mainButtonClicked', onSendData);
-    };
-  }, [onSendData]);
+      tg.offEvent('mainButtonClicked', onSendData)
+    }
+  }, [onSendData])
 
   const onAdd = (product) => {
-    const alreadyAdded = addedItems.find((item) => item.id === product.id);
+    const alreadyAdded = addedItems.find(item => item.id === product.id);
     let newItems = [];
 
-    if (alreadyAdded) {
-      newItems = addedItems.filter((item) => item.id !== product.id);
+    if(alreadyAdded) {
+      newItems = addedItems.filter(item => item.id !== product.id);
     } else {
       newItems = [...addedItems, product];
     }
 
-    setAddedItems(newItems);
+    setAddedItems(newItems)
 
-    if (newItems.length === 0) {
+    if(newItems.length === 0) {
       tg.MainButton.hide();
     } else {
       tg.MainButton.show();
       tg.MainButton.setParams({
-        text: `Купить ${getTotalPrice(newItems)}`,
-      });
+        text: `Купить ${getTotalPrice(newItems)}`
+      })
     }
-  };
-
-  const onRemove = (productId) => {
-    const updatedItems = addedItems.filter((item) => item.id !== productId);
-    setAddedItems(updatedItems);
-
-    if (updatedItems.length === 0) {
-      tg.MainButton.hide();
-    } else {
-      tg.MainButton.setParams({
-        text: `Купить ${getTotalPrice(updatedItems)}`,
-      });
-    }
-  };
-
-  useEffect(() => {
-    // Фильтруем товары по категории
-    let filtered = [];
-    if (activeCategory === 'Новое') {
-      // Возвращаем все товары, независимо от категории
-      filtered = products.slice().sort((a, b) => b.id - a.id);
-    } else {
-      // Возвращаем товары только выбранной категории
-      filtered = products.filter((product) => product.category === activeCategory);
-    }
-    setFilteredProducts(filtered);
-  }, [activeCategory]);
+  }
 
   return (
-      <div className={'product-list'}>
-        <div className={'categories'}>
-          <button onClick={() => setActiveCategory('Новое')} className={activeCategory === 'Новое' ? 'active' : ''}>
-            Новое
-          </button>
-          <button onClick={() => setActiveCategory('Кроссовки')} className={activeCategory === 'Кроссовки' ? 'active' : ''}>
-            Кроссовки
-          </button>
-          <button onClick={() => setActiveCategory('Одежда')} className={activeCategory === 'Одежда' ? 'active' : ''}>
-            Одежда
-          </button>
-        </div>
-        <div className={'product-list-items'}>
-          {filteredProducts.map((item) => (
-              <ProductItem key={item.id} product={item} onAdd={onAdd} onRemove={onRemove} className={'product-item'} />
-          ))}
-        </div>
+      <div className={'list'}>
+        {products.map(item => (
+            <ProductItem
+                product={item}
+                onAdd={onAdd}
+                className={'item'}
+            />
+        ))}
       </div>
   );
 };
