5d0b7a760772b0d698fdede484ac9bcd993d6396
vmay <va>
2024-05-12 19:26:46 +0300

    changed

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -1,23 +1,72 @@
-import React from 'react';
-import Button from "../Button/Button";
+import React, { useState, useEffect } from 'react';
+import Button from '../Button/Button';
 import './ProductItem.css';
 
-const ProductItem = ({product, className, onAdd}) => {
+const ProductItem = ({ product, className, onAdd, onRemove }) => {
+    const [selectedSize, setSelectedSize] = useState(null);
+    const [selectedProduct, setSelectedProduct] = useState(null);
+    const [inCart, setInCart] = useState(false);
+    const [minPrice, setMinPrice] = useState(null);
+
+    useEffect(() => {
+        if (selectedSize) {
+            setSelectedProduct({ ...product, selectedSize, price: product.prices[selectedSize] });
+        } else {
+            setSelectedProduct(null);
+        }
+
+        // Вычисляем минимальную цену среди всех размеров
+        const prices = Object.values(product.prices);
+        setMinPrice(Math.min(...prices));
+    }, [product, selectedSize]);
+
+    const onSizeSelect = (size) => {
+        setSelectedSize(size);
+    };
 
     const onAddHandler = () => {
-        onAdd(product);
-    }
+        if (selectedProduct) {
+            onAdd(selectedProduct);
+            setInCart(true);
+        } else {
+            alert('Выберите размер');
+        }
+    };
+
+    const onRemoveHandler = () => {
+        if (selectedProduct) {
+            onRemove(selectedProduct.id);
+            setInCart(false);
+        }
+    };
 
     return (
         <div className={'product ' + className}>
-            <div className={'img'}/>
+            <div className={'img'}>
+                <img src={product.img} alt={product.title} />
+            </div>
             <div className={'title'}>{product.title}</div>
             <div className={'description'}>{product.description}</div>
             <div className={'price'}>
-                <span>Стоимость: <b>{product.price}</b></span>
+                <span><b>{selectedSize ? product.prices[selectedSize] : `От ${minPrice}`}</b></span>
             </div>
-            <Button className={'add-btn'} onClick={onAddHandler}>
-                Добавить в корзину
+            <div className={'sizes'}>
+                {product.sizes && (
+                    <div>
+                        Размеры: {product.sizes.map((size) => (
+                        <button
+                            key={size}
+                            onClick={() => onSizeSelect(size)}
+                            className={selectedSize === size ? 'selected' : ''}
+                        >
+                            {size}
+                        </button>
+                    ))}
+                    </div>
+                )}
+            </div>
+            <Button className={'add-btn'} onClick={inCart ? onRemoveHandler : onAddHandler}>
+                {inCart ? 'Убрать из корзины' : 'Добавить в корзину'}
             </Button>
         </div>
     );
