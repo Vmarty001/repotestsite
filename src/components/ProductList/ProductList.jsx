805afbbb3a0f556c24553130270d9ce8b69520bd
vmay <va>
2024-05-12 18:50:47 +0300

    changed

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -40,7 +40,7 @@ const ProductList = () => {
       totalPrice: getTotalPrice(addedItems),
       queryId,
     };
-    fetch('http://localhost:8000', {
+    fetch('http://45.89.188.162:8000/web-data', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
