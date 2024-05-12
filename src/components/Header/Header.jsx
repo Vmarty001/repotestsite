87d8bc134ce48a181ccd3adcd69bb245f5bdcc84
vmay <va>
2024-05-12 19:16:44 +0300

    changed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,22 +2,16 @@ import React from 'react';
 import Button from "../Button/Button";
 import {useTelegram} from "../../hooks/useTelegram";
 import './Header.css';
-import Logo from './Logo';
 
 const Header = () => {
     const {user, onClose} = useTelegram();
 
     return (
-        <div>
-            <div className={'logo'}>
-                <Logo />
-            </div>
-            <div className={'header'}>
-
-                <span className={'username'}>
-                    {user?.username}
-                </span>
-            </div>
+        <div className={'header'}>
+            <Button onClick={onClose}>Закрыть</Button>
+            <span className={'username'}>
+                {user?.username}
+            </span>
         </div>
     );
 };
