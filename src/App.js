35522cbb8a62d8c5d7f8899d7e26fb2d4e42aaa5
vmay <va>
2024-05-14 22:15:23 +0300

    changed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React, { useEffect } from 'react';
 import './App.css';
-import { useTelegram } from "./hooks/useTelegram";
+import {useEffect} from "react";
+import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
+import {Route, Routes} from 'react-router-dom'
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
 
 function App() {
-    const { tg } = useTelegram();
+    const {onToggleButton, tg} = useTelegram();
 
     useEffect(() => {
         tg.ready();
-    }, [tg]);
+    }, [])
 
     return (
         <div className="App">
             <Header />
-            {/* Отображение формы */}
-            <Form />
-            {/* Отображение списка продуктов */}
-            <ProductList />
+            <Routes>
+                <Route index element={<ProductList />}/>
+                <Route path="/form" element={<Form />} />
+            </Routes>
         </div>
     );
 }
